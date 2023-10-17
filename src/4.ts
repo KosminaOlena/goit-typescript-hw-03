74c1c6f55d820e9cf8d65093ab268259e98446c1
KosminaOlena <113665261+KosminaOlena@users.noreply.github.com>
2023-10-17 10:53:48 +0300

    update

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random()
+    }
+    getSignature(){
+        return this.signature;
+    }
+}
+class Person{
+    constructor(private key: Key){}
+    getKey(){
+        return this.key;
+    }
+}
+abstract class House{
+    protected door: true|false;
+    protected key: Key;
+    public tenants: Person[] = [];
+    abstract openDoor(key: Key): void;
+
+    constructor(key: Key){
+        this.key = key;
+        this.door = false;
+    }
+    
+    comeIn(tenant: Person){
+        if(this.door){
+           this.tenants.push(tenant);
+        }
+    }
+}
+class MyHouse extends House{
+    openDoor(key: Key): void {
+        if(key.getSignature() === this.key.getSignature()){
+            this.door=true;
+        }   
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
